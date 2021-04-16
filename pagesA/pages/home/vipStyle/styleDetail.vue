366c4677adaad7965fe5519071cdd5784a255fbe
Ambler <534089917@qq.com>
2021-04-16 17:41:31 +0800

    Ambler

--- pagesA/pages/home/vipStyle/styleDetail.vue
+++ pagesA/pages/home/vipStyle/styleDetail.vue
@@ -1,18 +1,20 @@
 <template>
 	<view>
 		<view class="videoBox">
-			<video id="videos" class="videos" :src="videoUrl" @error="videoErrorCallback" controls
-				:show-center-play-btn="false" :poster="fengmian" @play="playVideo" show-fullscreen-btn>
-				<cover-image class="img" v-if="playBtnShow" @click="playVideo"
-					src="/pagesA/static/img/vipStyle/play.png"></cover-image>
-			</video>
-			<view>
-				<view class="contentText">
-					{{title}}
+			<view class="video_info">
+				<!-- 视频 -->
+				<view class="video_video">
+					<video id='myVideos' show-fullscreen-btn data-id="myVideos" :src="videoUrl"
+						controls></video>
 				</view>
-				<view class="controls">
-					<view class="date">{{createtime}}</view>
-					<view class="controlsItem">
+				<view class="allText">
+					<view class="contentText">
+						{{title}}
+					</view>
+					<view class="controls">
+						<view class="date">{{createtime}}</view>
+						<view class="controlsItem">
+						</view>
 					</view>
 				</view>
 			</view>
@@ -28,7 +30,6 @@
 				fengmian: "",
 				createtime: "",
 				title: "",
-				playBtnShow: true,
 			};
 		},
 		onLoad(option) {
@@ -47,97 +48,90 @@
 					"&createtime=" + this.createtime
 			}
 		},
-		methods: {
-			/**
-			 * @param {Object} e
-			 * 播放
-			 */
-			playVideo() {
-				this.playBtnShow = false
-				uni.createVideoContext("videos", this).play()
-			},
-			/**
-			 * @param {Object} e
-			 * 视频错误
-			 */
-			videoErrorCallback: function(e) {
-				console.log('视频错误信息:')
-				console.log(e.target.errMsg)
-			},
-		}
+		methods: {}
 	}
 </script>
 
 <style lang="scss">
 	.videoBox {
-		margin: 10rpx 0rpx;
-		border: 1px solid #efefef;
-		border-radius: 12rpx;
-		overflow: hidden;
-		box-shadow: 0px 0px 2px #efefef;
-		position: relative;
-		background: #fff;
+		width: 100%;
 
-		.videos {
+		.video_info {
 			width: 100%;
-			height: 85vh;
+			height: 100vh;
+			// margin: 10rpx 0rpx;
+			border: 1px solid #efefef;
+			border-radius: 12rpx;
+			overflow: hidden;
+			box-shadow: 0px 0px 2px #efefef;
 			position: relative;
-
-			.img {
-				position: absolute;
-				width: 100rpx;
-				height: 100rpx;
-				left: 45vw;
-				top: 45vh;
-			}
-		}
-
-		.contentText {
-			background: #fff;
-			padding: 20rpx 30rpx;
-			font-size: 28rpx;
-			font-weight: 600;
-			color: #333333;
-			word-wrap: break-word;
-		}
-
-		.controls {
 			background: #fff;
 			display: flex;
-			padding: 8rpx 30rpx 20rpx 30rpx;
-			justify-content: space-between;
-
-			.date {
-				font-size: 22rpx;
-				color: #999999;
-			}
-
-			.controlsItem {
-				display: flex;
-
-				.items {
-					display: flex;
-					padding: 0rpx 20rpx;
-					font-size: 18rpx;
-					color: #999999;
-
-					span {
-						line-height: 30rpx;
-					}
-				}
-
-				.items1 {
-					display: flex;
-					padding: 0rpx 20rpx;
-					font-size: 18rpx;
-					color: #999999;
-					border-right: 2px solid #999;
-
-					span {
-						line-height: 30rpx;
-					}
+			flex-direction: column;
+			/* 视频 */
+			.video_video {
+				width: 100%;
+				flex-grow: 1;
+				border-radius: 15upx;
+				
+				video {
+					width: 100%;
+					height: 100%;
+					border-radius: 15upx;
 				}
 			}
+			.allText{
+				height: 73px;
+				.contentText {
+					background: #fff;
+					padding: 20rpx 30rpx;
+					font-size: 28rpx;
+					font-weight: 600;
+					color: #333333;
+					word-wrap: break-word;
+				}
+				
+				.controls {
+					background: #fff;
+					display: flex;
+					padding: 8rpx 30rpx 20rpx 30rpx;
+					justify-content: space-between;
+				
+					.date {
+						font-size: 22rpx;
+						color: #999999;
+				
+					}
+				
+					.controlsItem {
+						display: flex;
+				
+						.items {
+							display: flex;
+							padding: 0rpx 20rpx;
+							font-size: 18rpx;
+							color: #999999;
+				
+							span {
+								line-height: 30rpx;
+							}
+						}
+				
+						.items1 {
+							display: flex;
+							padding: 0rpx 20rpx;
+							font-size: 18rpx;
+							color: #999999;
+							border-right: 2px solid #999;
+				
+							span {
+								line-height: 30rpx;
+							}
+						}
+					}
+				}
+			}
+			
 		}
 	}
 </style>
--- pagesA/pages/home/vipStyle/vipStyle.vue
+++ pagesA/pages/home/vipStyle/vipStyle.vue
@@ -1,5 +1,6 @@
 <template>
-	<view class="all">
+	<view class="">
+		<!-- 热门视频 -->
 		<view class="searchBox">
 			<view class="inputBox">
 				<u-search class="search" v-model="searchPre.title" placeholder="请输入关键字" bg-color="#f2f2f2" clearabled
@@ -7,32 +8,39 @@
 					@clear="search"></u-search>
 			</view>
 		</view>
-		<view style="margin-top: 44px;">
-			<view class="videoBox" v-for="(item,index) in videoList">
-				<video class="videos" :id="item.fengcaiId" :src="item.shipinurl" @error="videoErrorCallback" controls
-					show-fullscreen-btn :show-center-play-btn="false" :poster="item.fengmian"
-					@play="playVideo(item.fengcaiId,index)" @fullscreenchange="fullscreenchanges">
-					<cover-image class="img" v-if="(index!==clickIndex)&&playBtnShow"
-						@click="playVideo(item.fengcaiId,index)" src="/pagesA/static/img/vipStyle/play.png">
-					</cover-image>
-				</video>
-				<view @click="toDetail(item.shipinurl,item.fengmian,item.title,item.createtime)">
-					<view class="contentText">
-						{{item.title}}
+		<view class="hot_video" style="margin-top: 44px;">
+
+			<!-- 热门视频 -->
+			<view class="videoBox">
+				<view class="video_info" v-for="(item, index) in hot_video" :key="index">
+					<!-- 图片 -->
+					<view v-if="item.isShowImage" class="video_img">
+						<image class="fm" :src="item.fengmian" mode=""></image>
+						<view class="anNiuBox">
+							<image @tap="boFang($event,item)" :data-id="item.fengcaiId" class="anNiu"
+								src="/pagesA/static/img/vipStyle/play.png" mode=""></image>
+						</view>
 					</view>
-					<view class="controls">
-						<view class="date">{{item.createtime}}</view>
-						<view class="controlsItem">
-							<!-- <view class="items" @click="wxShare(item.fengmian,item.title,item.shipinurl)">
-								<u-image width="32rpx" height="32rpx" src="/pagesA/static/img/vipStyle/share.png"></u-image>
-							</view> -->
+					<!-- 视频 -->
+					<view v-else class="video_video">
+						<video @pause="pauseVideo" @play='playVideo' :id='`t${item.fengcaiId}`'
+							:data-id="item.fengcaiId" :src="item.shipinurl"
+							controls show-fullscreen-btn :show-center-play-btn="false"></video>
+					</view>
+					<view @click="toDetail(item.shipinurl,item.fengmian,item.title,item.createtime)">
+						<view class="contentText">
+							{{item.title}}
+						</view>
+						<view class="controls">
+							<view class="date">{{item.createtime}}</view>
+							<view class="controlsItem">
+							</view>
 						</view>
 					</view>
 				</view>
 			</view>
 		</view>
-		<u-empty style="height: 180px;" v-if="emptyShow" text="暂无数据" mode="list"></u-empty>
-		<u-loadmore v-if="loadShow" :status="status" :load-text="loadText" />
+
 	</view>
 </template>
 
@@ -40,31 +48,22 @@
 	export default {
 		data() {
 			return {
-				playBtnShow: true,
+				isVideo: 1, //判断到底显示视频还是图片
+				newPerson: false,
+				hot_video: [], //热门视频
 				searchPre: {
 					title: "",
 				},
-				beforeVid: "",
-				videoContext: {},
-				videoList: [],
-				clickIndex: -1,
-				loadShow: false,
 				pageNo: 1,
 				pageSize: 10,
 				total: 0,
-				emptyShow: false,
-				status: 'loadmore',
-				loadText: {
-					loadmore: '上拉加载更多',
-					loading: '努力加载中',
-					nomore: '实在没有了'
-				}
-			}
+			};
 		},
+
 		//上拉触底事件
 		onReachBottom() {
 			this.loadShow = true
-			const len = this.videoList.length;
+			const len = this.hot_video.length;
 			if (len < this.total) {
 				this.status = 'loading'
 					++this.pageNo
@@ -77,16 +76,7 @@
 				}, 1000)
 			}
 		},
-		onLoad(option) {
-			if (option.fengcaiId) {}
-		},
-		/**
-		 * @param {Object} e
-		 * 输入框输入时
-		 */
-		onNavigationBarSearchInputChanged(e) {
-			this.searchPre.title = e.text;
-		},
+
 		/**
 		 * 按钮回车时
 		 */
@@ -101,11 +91,8 @@
 			this.search()
 		},
 
-		/**
-		 * 搜索按钮
-		 */
-		onNavigationBarButtonTap() {
-			this.search()
+		created() {
+			this.getVideoList()
 		},
 		methods: {
 			/**
@@ -113,6 +100,11 @@
 			 * 前往详情
 			 */
 			toDetail(videoUrl, fengmian, title, createtime) {
+				this.hot_video.forEach(item => {
+					let temp = item.fengcaiId
+					uni.createVideoContext('t' + temp).pause();
+					uni.createVideoContext('t' + temp).seek(0)
+				})
 				uni.navigateTo({
 					url: "./styleDetail?videoUrl=" + videoUrl + "&fengmian=" + fengmian + "&title=" + title +
 						"&createtime=" + createtime
@@ -123,7 +115,7 @@
 			 */
 			search() {
 				this.pageNo = 1
-				this.videoList = []
+				this.hot_video = []
 				this.getVideoList()
 			},
 			/**
@@ -138,99 +130,55 @@
 					title: that.searchPre.title,
 				}
 				that.$u.api.getVipStyle(obj).then(res => {
+					res.list.forEach(item => {
+						item.isShowImage = true;
+					})
 					let list = res.list
 					that.total = res.total
-					that.videoList = that.videoList.concat(list)
+					that.hot_video = that.hot_video.concat(list)
 					res.total === 0 ? this.emptyShow = true : this.emptyShow = false
 					that.loadShow = false
 				})
 			},
-			/**
-			 * @param {Object} e
-			 * 播放
-			 */
-			playVideo(e, index) {
-				if (this.beforeVid !== e) {
-					uni.createVideoContext(`${this.beforeVid}`, this).pause()
-					uni.createVideoContext(`${this.beforeVid}`, this).seek(0)
-				}
-				this.clickIndex = index
-				this.beforeVid = e
-				uni.createVideoContext(`${e}`, this).play()
+
+
+			/* 视频暂停 */
+			pauseVideo() {
+				//console.log('点击了暂停');
+				//this.isVideo=2
 			},
-			/**
-			 * @param {Object} e
-			 * 全屏 非全屏
-			 */
-			fullscreenchanges(e) {
-				if (e.detail.fullScreen) {
-					this.playBtnShow = false
-				} else {
-					this.playBtnShow = true
-				}
+			/* 视频播放 */
+			playVideo(e) {
+				let currentId = e.target.dataset.id
+				this.hot_video.forEach(item => {
+					let temp = item.fengcaiId
+					if (temp !== currentId) {
+						uni.createVideoContext('t' + temp).pause();
+						uni.createVideoContext('t' + temp).seek(0)
+					}
+				})
+			},
+			/* 点击图片的播放按钮 */
+			boFang(e, item) {
+				let currentId = e.target.dataset.id
+				item.isShowImage = false;
+				this.hot_video.forEach(items => {
+					let temp = items.fengcaiId
+					if (temp !== currentId) {
+						uni.createVideoContext('t' + temp).pause();
+						uni.createVideoContext('t' + temp).seek(0)
+					}
+				})
+				uni.createVideoContext('t' + currentId).play();
 			},
 
-			/**
-			 * @param {Object} img
-			 * @param {Object} text
-			 * @param {Object} url
-			 * 微信分享
-			 * 需要appid 才可运行 
-			 */
-			// wxShare(img, text, url) {
-			// 	// #ifdef APP-PLUS
-			// 	uni.share({
-			// 		provider: "weixin",
-			// 		scene: "WXSceneSession",
-			// 		type: 4,
-			// 		mediaUrl: url,
-			// 		title: "青企协",
-			// 		summary: text,
-			// 		imageUrl: img, //可不填
-			// 		success: function(res) {
-			// 			console.log("success:" + JSON.stringify(res));
-			// 		},
-			// 		fail: function(err) {
-			// 			console.log("fail:" + JSON.stringify(err));
-			// 		}
-			// 	});
-			// 	// #endif
-			// 	// #ifdef MP-WEIXIN
-			// 	// #endif
-			// 	// #ifdef H5
 
-			// 	// #endif
 
-			// },
-			/**
-			 * @param {Object} e
-			 * 视频错误
-			 */
-			videoErrorCallback: function(e) {
-				console.log('视频错误信息:')
-				console.log(e.target.errMsg)
-			},
-		},
-		created() {
-			this.getVideoList()
-		},
-		mounted() {
 
 		}
-
-	}
+	};
 </script>
-
 <style lang="scss">
-	page {
-		background-color: #f2f2f2;
-	}
-
-	cover-view,
-	cover-image {
-		display: inline-block;
-	}
-
 	.searchBox {
 		position: fixed;
 		// #ifdef APP-PLUS
@@ -244,15 +192,15 @@
 		// #endif
 		width: -webkit-fill-available;
 		right: 0px;
-		z-index: 99;
+		z-index: 999;
 		background-color: #ffffff;
 		display: flex;
 		padding: 8rpx 10rpx;
-
+	
 		.sxBox {
 			width: 120rpx;
 		}
-
+	
 		.inputBox {
 			flex-grow: 1;
 			display: flex;
@@ -260,78 +208,185 @@
 			justify-content: space-around;
 		}
 	}
+	.yinYing2 {
+		width: 100%;
+		position: absolute;
+		height: 80upx;
+		// background-image: url(../../static/yinYing.png);
+		background-size: 100% 100%;
+		z-index: 444;
+		transform: rotate(180deg);
+		bottom: 0;
+		display: flex;
+		align-items: center;
+
+		/* 头像昵称 */
+		.touXiang {
+			height: 45upx;
+			display: flex;
+			z-index: 666;
+			color: #FFFFFF;
+			font-size: 28upx;
+			transform: rotate(180deg);
+			width: 320rpx;
+
+			image {
+				width: 41upx;
+				height: 41upx;
+				border-radius: 50%;
+				margin-right: 10upx;
+			}
+		}
+
+	}
 
 
-	.all {
-		padding: 0 30rpx;
+
+
+	// 热门视频
+	.hot_video {
+		width: 95vw;
+		margin: 0 auto;
 
 		.videoBox {
-			margin: 10rpx 0rpx;
-			border: 1px solid #efefef;
-			border-radius: 12rpx;
-			overflow: hidden;
-			box-shadow: 0px 0px 2px #efefef;
-			position: relative;
-			background: #fff;
+			width: 100%;
 
-			.videos {
+			.video_info {
 				width: 100%;
-				height: 380rpx;
+				margin: 10rpx 0rpx;
+				border: 1px solid #efefef;
+				border-radius: 12rpx;
+				overflow: hidden;
+				box-shadow: 0px 0px 2px #efefef;
 				position: relative;
-
-				.img {
-					position: absolute;
-					width: 100rpx;
-					height: 100rpx;
-					left: 39vw;
-					top: 7vh;
-				}
-			}
-
-			.contentText {
 				background: #fff;
-				padding: 20rpx 30rpx;
-				font-size: 28rpx;
-				font-weight: 600;
-				color: #333333;
-				word-wrap: break-word;
-			}
 
-			.controls {
-				background: #fff;
-				display: flex;
-				padding: 8rpx 30rpx 20rpx 30rpx;
-				justify-content: space-between;
+				/* 视频 */
+				.video_video {
+					width: 100%;
+					height: 400upx;
+					border-radius: 15upx;
 
-				.date {
-					font-size: 22rpx;
-					color: #999999;
-
+					video {
+						width: 100%;
+						height: 400upx;
+						border-radius: 15upx;
+					}
 				}
 
-				.controlsItem {
-					display: flex;
+				/* 视频图片 */
+				.video_img {
+					width: 100%;
+					height: 400upx;
+					position: relative;
+					border-radius: 15upx;
+					overflow: hidden;
+					box-shadow: 0 0 10upx #ccc;
+					position: relative;
+					overflow: hidden;
 
-					.items {
+					.fm {
+						width: 100%;
+						position: absolute;
+						top: 50%;
+						left: 50%;
+						transform: translate(-50%, -50%);
+					}
+
+					.anNiuBox {
+						position: absolute;
+						z-index: 666;
+						margin: auto;
+						top: 0;
+						bottom: 0;
+						left: 0;
+						right: 0;
+						width: 90upx;
+						height: 90upx;
 						display: flex;
-						padding: 0rpx 20rpx;
-						font-size: 18rpx;
-						color: #999999;
+						flex-direction: column;
+						justify-content: space-between;
+						align-items: center;
 
-						span {
-							line-height: 30rpx;
+						.anNiu {
+							width: 80upx;
+							height: 80upx;
+							border-radius: 50%;
 						}
 					}
 
-					.items1 {
+					.yanBox {
+						position: absolute;
+						z-index: 666;
+						top: 25upx;
 						display: flex;
-						padding: 0rpx 20rpx;
-						font-size: 18rpx;
-						color: #999999;
-						border-right: 2px solid #999;
+						align-items: center;
+						font-size: 26upx;
 
-						span {
-							line-height: 30rpx;
+						.yan {
+							width: 24upx;
+							height: 20upx;
+							margin-right: 10upx;
+							margin-left: 10rpx;
+						}
+					}
+
+					.user_avatar {
+						width: 41upx;
+						height: 41upx;
+						border-radius: 50%;
+						position: absolute;
+						left: 15upx;
+						bottom: 15upx;
+						z-index: 666;
+					}
+				}
+
+				.contentText {
+					background: #fff;
+					padding: 20rpx 30rpx;
+					font-size: 28rpx;
+					font-weight: 600;
+					color: #333333;
+					word-wrap: break-word;
+				}
+
+				.controls {
+					background: #fff;
+					display: flex;
+					padding: 8rpx 30rpx 20rpx 30rpx;
+					justify-content: space-between;
+
+					.date {
+						font-size: 22rpx;
+						color: #999999;
+
+					}
+
+					.controlsItem {
+						display: flex;
+
+						.items {
+							display: flex;
+							padding: 0rpx 20rpx;
+							font-size: 18rpx;
+							color: #999999;
+
+							span {
+								line-height: 30rpx;
+							}
+						}
+
+						.items1 {
+							display: flex;
+							padding: 0rpx 20rpx;
+							font-size: 18rpx;
+							color: #999999;
+							border-right: 2px solid #999;
+
+							span {
+								line-height: 30rpx;
+							}
 						}
 					}
 				}
